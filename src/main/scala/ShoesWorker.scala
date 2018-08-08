82c6952a01ea995426bab9b239046a97d8129775
oleg.iermilov@globallogic.com <oleg.iermilov@globallogic.com>
2018-08-08 10:17:12 +0300

    Recursive solution

--- src/main/scala/ShoesWorker.scala
+++ src/main/scala/ShoesWorker.scala
@@ -2,22 +2,29 @@ import scala.io._
 
 object ShoesWorker {
   def main(args: Array[String]): Unit = {
-    val pipeline = StdIn.readLine("Enter the shoes pipeline:")
-
-    println("The number of pairs collected is " + countPairs(pipeline))
-
+    while (true) {
+      val pipeline = StdIn.readLine("Enter the shoes pipeline:")
+      if (pipeline == "exit") sys.exit(0)
+      println(
+        "The number of pairs collected is " + plantWorker(pipeline, 0, 0, 0, 0))
+    }
   }
 
-  def countPairs(p: String ): Int = {
-    var cnt, status = 0
-    for (c <- p) {
-      if (status == 0) {
-        status = c.toInt
-      } else if (status + c.toInt == 158) {
-        cnt += 1
-        status = 0
-      } else return cnt
-    }
-    return cnt
+  def plantWorker(input: String,
+                  pos: Integer,
+                  qSize: Int,
+                  qType: Char,
+                  counter: Int): Int = {
+    // Evaluate state of the queue with shoes that were put aside and the counter
+    val (qs, qt, cnt) = if (qSize > 0) {
+      if (input(pos) == qType) (qSize + 1, qType, counter)
+      else if (qSize == 1) (0, qType, counter + 1)
+      else (qSize - 1, qType, counter)
+    } else (1, input(pos), counter)
+
+    // Recursively call the function until reach the end of the input
+    if (pos == input.length - 1) {
+      return cnt
+    } else return plantWorker(input, pos + 1, qs, qt, cnt)
   }
 }
